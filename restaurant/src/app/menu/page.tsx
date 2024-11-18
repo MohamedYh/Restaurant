a7dc22c91f57a7d1a8410fc61d3ce43f8ca4345a
MohamedYh <mohamedadyh00@gmail.com>
2024-11-18 23:38:54 +0100

    18/11/2024 update

--- restaurant/src/app/menu/page.tsx
+++ restaurant/src/app/menu/page.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/footer";
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 import { themes_color, themes_text_color } from "../page";
+import { MenuData } from "../../../public/Menu/data";
 
 function Menu() {
     const theme_index = useSelector((state: RootState) => state.slice.theme);
@@ -13,8 +14,8 @@ function Menu() {
     return (
         <div
             style={{
-                backgroundColor: themes_color[theme_index],
-                color: themes_text_color[theme_index],
+                backgroundColor: themes_color[1],
+                color: themes_text_color[1],
             }}
             className="menu_main"
         >
@@ -27,27 +28,112 @@ function Menu() {
                 </div>
                 <div className="menu_containers_parent">
                     <div className="left_menu_container">
-                        <div className="menu_child"></div>
-                        <div className="menu_child"></div>
-                        <div className="menu_child"></div>
+                        <div id="pizza" className="menu_child">
+                            <h2>Pizza</h2>
+                            <div>
+                                {MenuData.pizza.map((v, i) => {
+                                    return (
+                                        <div>
+                                            <p>{v.name}</p>
+                                            <p>${v.price}</p>
+                                        </div>
+                                    );
+                                })}
+                                k
+                            </div>
+                        </div>
                         <div className="preview_image">
                             <img src="" alt="" />
                         </div>
-                        <div className="menu_child"></div>
-                        <div className="menu_child"></div>
+                        <div id="sandwiches" className="menu_child">
+                            <h2>Sandwiches</h2>
+                            <div>
+                                {MenuData.sandwiches.map((v, i) => {
+                                    return (
+                                        <div>
+                                            <p>{v.name}</p>
+                                            <p>${v.price}</p>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        </div>
+                        <div className="preview_image">
+                            <img src="" alt="" />
+                        </div>
+                        <div id="juices" className="menu_child">
+                            <h2>Juices</h2>
+                            <div>
+                                {MenuData.juices.map((v, i) => {
+                                    return (
+                                        <div>
+                                            <p>{v.name}</p>
+                                            <p>${v.price}</p>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        </div>
+                        <div id="desserts" className="menu_child">
+                            <h2>Desserts</h2>
+                            <div>
+                                {MenuData.desserts.map((v, i) => {
+                                    return (
+                                        <div>
+                                            <p>{v.name}</p>
+                                            <p>${v.price}</p>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        </div>
                     </div>
                     <div className="right_menu_container">
                         <div className="preview_image_container">
                             <img src="" alt="" />
                         </div>
-                        <div className="preview_image_container">
-                            <img src="" alt="" />
+                        <div id="burgers" className="menu_child">
+                            <h2>Burgers</h2>
+                            <div>
+                                {MenuData.burgers.map((v, i) => {
+                                    return (
+                                        <div>
+                                            <p>{v.name}</p>
+                                            <p>${v.price}</p>
+                                        </div>
+                                    );
+                                })}
+                            </div>
                         </div>
                         <div className="preview_image">
                             <img src="" alt="" />
                         </div>
-                        <div className="menu_child"></div>
-                        <div className="menu_child"></div>
+                        <div id="salads" className="menu_child">
+                            <h2>Salads</h2>
+                            <div>
+                                {MenuData.salads.map((v, i) => {
+                                    return (
+                                        <div>
+                                            <p>{v.name}</p>
+                                            <p>${v.price}</p>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        </div>
+                        <div id="french_fries" className="menu_child">
+                            <h2>French Fries</h2>
+                            <div>
+                                {MenuData.french_fries.map((v, i) => {
+                                    return (
+                                        <div>
+                                            <p>{v.name}</p>
+                                            <p>${v.price}</p>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        </div>
                         <div className="preview_image">
                             <img src="" alt="" />
                         </div>
